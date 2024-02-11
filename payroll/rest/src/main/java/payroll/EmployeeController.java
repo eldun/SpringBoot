c0b5ae8bec51ca2272df1a98e0347b6e3fc851b1
dunneev <dunneev@gmail.com>
2024-02-10 23:52:45 -0500

    Update /employees to return CollectionModel

--- payroll/rest/src/main/java/payroll/EmployeeController.java
+++ payroll/rest/src/main/java/payroll/EmployeeController.java
@@ -25,13 +25,18 @@ class EmployeeController {
   }
 
 
-  // Aggregate root
-  // tag::get-aggregate-root[]
+  // Aggregate root resource
   @GetMapping("/employees")
-  List<Employee> all() {
-    return repository.findAll();
+  CollectionModel<EntityModel<Employee>> all() {
+  
+    List<EntityModel<Employee>> employees = repository.findAll().stream()
+        .map(employee -> EntityModel.of(employee,
+            linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+            linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+        .collect(Collectors.toList());
+  
+    return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
   }
-  // end::get-aggregate-root[]
 
   @PostMapping("/employees")
   Employee newEmployee(@RequestBody Employee newEmployee) {
