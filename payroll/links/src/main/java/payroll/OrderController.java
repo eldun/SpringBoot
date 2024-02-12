379130c6e0e671f10736ea68f5c833d003a7e70c
dunneev <dunneev@gmail.com>
2024-02-12 15:13:17 -0500

    Add "complete order" operation

--- payroll/links/src/main/java/payroll/OrderController.java
+++ payroll/links/src/main/java/payroll/OrderController.java
@@ -63,6 +63,25 @@ class OrderController {
                 .body(assembler.toModel(newOrder));
     }
 
+    @PutMapping("/orders/{id}/complete")
+    ResponseEntity<?> complete(@PathVariable Long id) {
+
+        Order order = orderRepository.findById(id) //
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.COMPLETED);
+            return ResponseEntity.ok(assembler.toModel(orderRepository.save(order)));
+        }
+
+        return ResponseEntity //
+                .status(HttpStatus.METHOD_NOT_ALLOWED) //
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE) //
+                .body(Problem.create() //
+                        .withTitle("Method not allowed") //
+                        .withDetail("You can't complete an order that is in the " + order.getStatus() + " status"));
+    }
+
     @DeleteMapping("/orders/{id}/cancel")
     ResponseEntity<?> cancel(@PathVariable Long id) {
 
