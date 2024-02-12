dc372eaa5bcfc2e497389aa56ee8a7500f4e3c11
dunneev <dunneev@gmail.com>
2024-02-12 14:49:29 -0500

    Update PUT method

--- payroll/evolution/src/main/java/payroll/EmployeeController.java
+++ payroll/evolution/src/main/java/payroll/EmployeeController.java
@@ -65,18 +65,24 @@ class EmployeeController {
   }
 
   @PutMapping("/employees/{id}")
-  Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+  ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
 
-    return repository.findById(id)
+    Employee updatedEmployee = repository.findById(id) //
         .map(employee -> {
           employee.setName(newEmployee.getName());
           employee.setRole(newEmployee.getRole());
           return repository.save(employee);
-        })
+        }) //
         .orElseGet(() -> {
           newEmployee.setId(id);
           return repository.save(newEmployee);
         });
+  
+    EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+  
+    return ResponseEntity //
+        .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+        .body(entityModel);
   }
 
   @DeleteMapping("/employees/{id}")
